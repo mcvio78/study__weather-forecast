9afc8f9843da1733dbcd7a7afcb969eada61990f
Mauro <mcvio78@gmail.com>
2020-10-19 19:45:05 +0200

    React Router starting setup.

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from "react";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 import Layout from "./hoc/Layout/Layout";
 import SelectionPage from "./containers/SelectionPage/SelectionPage";
 
 const App = () => {
+  const routes = (
+    <Switch>
+      <Route path="/" exact component={SelectionPage} />
+      <Redirect to="/" />
+    </Switch>
+  );
+
   return (
     <div>
-      <Layout>
-        <SelectionPage />
-      </Layout>
+      <Layout>{routes}</Layout>
     </div>
   );
 };
